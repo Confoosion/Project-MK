7f99e9ab2912caadec69256143fd541adad62d0f
Jake <jakesales@gmail.com>
2025-09-23 23:13:28 -0700

    Created Shotgun character
    Consists of 3 hitboxes that do 1/3 damage, so if the enemy is close it'll hit all 3 hitboxes. Also made a particle effect for it, but it's strictly visual.

---
+++ Assets/Scripts/Characters/ShotgunCharacter.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Characters/Shotgun Character")]
+public class ShotgunCharacter : CharacterSO
+{
+    public override void UseWeapon(Transform origin)
+    {
+        float direction = (origin.localScale.x == 1) ? -1f : 1f;
+        GameObject atk = Instantiate(attackObject, origin.position + new Vector3(direction * 0.5f, 0f, 0f), Quaternion.Euler(new Vector3(0f, 0f, (direction == -1) ? 0f : 180f)));
+        atk.GetComponent<MeleeAttack>().GetData(attackDuration, attackPower / 3f);
+    }
+}
