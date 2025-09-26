72b2c8c5046319e73e08018b0002c807e5ada352
Jake <jakesales@gmail.com>
2025-09-25 20:27:55 -0700

    Created freeze gun character (lazily)
    Basically just copy pasted the flamethrower character and changed the color of the particle lol.... Also changed the shotgun character's attack to actually hit with the particles instead of invisible hitboxes.

--- Assets/Scripts/Characters/ShotgunCharacter.cs
+++ Assets/Scripts/Characters/ShotgunCharacter.cs
@@ -7,6 +7,6 @@ public class ShotgunCharacter : CharacterSO
     {
         float direction = (origin.localScale.x == 1) ? -1f : 1f;
         GameObject atk = Instantiate(attackObject, origin.position + new Vector3(direction * 0.5f, 0f, 0f), Quaternion.Euler(new Vector3(0f, 0f, (direction == -1) ? 0f : 180f)));
-        atk.GetComponent<MeleeAttack>().GetData(attackDuration, attackPower / 3f);
+        atk.transform.GetChild(0).GetComponent<ParticleAttack>().GetData(attackDuration, origin);
     }
 }
