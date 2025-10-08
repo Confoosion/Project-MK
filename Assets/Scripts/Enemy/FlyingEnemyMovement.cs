111da748a6566b60fdcda21acf53fa6ca89afd1d
Calvin Hunter <70008107+CalvinHunter03@users.noreply.github.com>
2025-10-08 14:08:23 -0700

    started flying enemy

---
+++ Assets/Scripts/Enemy/FlyingEnemyMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class FlyingEnemyMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private float yRangeMax = 6.5f;
+    [SerializeField] private float yRangeMix = 3;
+    [SerializeField] private float xRange = 8;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
