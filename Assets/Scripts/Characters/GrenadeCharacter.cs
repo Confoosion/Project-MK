2e72a1b8af60bfa0ecf6b71f262a86e0ef9ca46d
Jake <jakesales@gmail.com>
2026-02-25 18:22:13 -0800

    Grenade attack upgrades implemented

--- Assets/Scripts/Characters/GrenadeCharacter.cs
+++ Assets/Scripts/Characters/GrenadeCharacter.cs
@@ -9,14 +9,25 @@ public class GrenadeCharacter : CharacterSO
 
     [SerializeField] float impactDamage;
     [SerializeField] float impactDuration;
+    [SerializeField] int burstCount = 1;
+    [SerializeField] float burstRate;
 
     public override void UseWeapon(Transform origin, PlayerAttack playerAttack)
     {
         float direction = (origin.localScale.x == 1) ? -1f : 1f;
-        GameObject atk = Instantiate(attackObject, origin.position + new Vector3(0f, 0.25f, 0f), Quaternion.Euler(new Vector3(0f, 0f, (direction == -1) ? 180f : 0f)));
 
-        atk.GetComponent<RangeAttack>().SetData(attackPower, destroyOnTerrain);
-        atk.GetComponent<RangeAttack>().SetImpactData(impactDamage, impactDuration);
-        atk.GetComponent<Rigidbody2D>().AddForce(new Vector3(direction, 0.5f, 0f) * projectileForce, ForceMode2D.Impulse);
+        if(burstCount <= 1)
+        {
+            GameObject atk = Instantiate(attackObject, origin.position + new Vector3(0f, 0.25f, 0f), Quaternion.Euler(new Vector3(0f, 0f, (direction == -1) ? 180f : 0f)));
+
+            atk.GetComponent<RangeAttack>().SetData(attackPower, destroyOnTerrain);
+            atk.GetComponent<RangeAttack>().SetImpactData(impactDamage, impactDuration);
+            atk.GetComponent<Rigidbody2D>().AddForce(new Vector3(direction, 0.5f, 0f) * projectileForce, ForceMode2D.Impulse);
+        }
+        else
+        {
+            playerAttack.BurstAttack(attackObject, burstCount, burstRate, 0f, attackPower, 
+                                     new Vector2(1f, 0.5f) * projectileForce, destroyOnTerrain, impactDamage, impactDuration);
+        }
     }
 }
