f01e9076d1a7b5a45c5b2320a983d66492add2ed
Jake <jakesales@gmail.com>
2026-03-19 15:36:22 -0700

    Keybind added to godmode
    Press i

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -63,6 +63,11 @@ public class PlayerControl : MonoBehaviour
 
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            godMode = !godMode;
+        }
+
         // Movement
         if (!movementLocked)
         {
