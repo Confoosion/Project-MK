3a1385cb0b7a3ea3681bb07c747ef865f59deab2
Jake <jakesales@gmail.com>
2026-03-26 12:51:55 -0700

    Added shop background assets
    Also fixed the prefab and grid layout component to fit with the background properly

--- Assets/Scripts/Shop/CharacterShopUI.cs
+++ Assets/Scripts/Shop/CharacterShopUI.cs
@@ -22,7 +22,7 @@ public class CharacterShopUI : MonoBehaviour
         characterSet = _set;
         UpdateDisplay();
 
-        upgradeButton.onClick.AddListener(OnUpgradeClicked);
+        // upgradeButton.onClick.AddListener(OnUpgradeClicked);
         // CharacterSetSO.CharacterUpgrade currChar = _set.upgrades[_set.currentLevel-1];
         
         // nameText.SetText(_set.itemName);
@@ -53,13 +53,14 @@ public class CharacterShopUI : MonoBehaviour
             {
                 priceText.SetText("MAX LEVEL");
                 upgradeButton.interactable = false;
+                upgradeButton.gameObject.SetActive(false);
             }
             else
             {
                 var nextUpgrade = characterSet.GetNextUpgrade();
                 if(nextUpgrade != null)
                 {
-                    priceText.SetText(currentUpgrade.upgradePrice.ToString() + " Currency");
+                    priceText.SetText(currentUpgrade.upgradePrice.ToString() + "c");
                     upgradeButton.interactable = true;
                 }
             }
@@ -74,7 +75,7 @@ public class CharacterShopUI : MonoBehaviour
         }
     }
 
-    private void OnUpgradeClicked()
+    public void OnUpgradeClicked()
     {
         if(characterSet.BuyItem())
             SoundManager.Singleton.PlayUIAudio(buySuccessful_SFX);
